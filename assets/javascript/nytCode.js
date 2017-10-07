029c881b9b49ae444f7dae31de0f1566f16c7442
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-10-07 13:24:16 -0400

    resolved conflicts

--- assets/javascript/nytCode.js
+++ assets/javascript/nytCode.js
@@ -1,66 +1,51 @@
-// SETUP VARIABLES
-// ==========================================================
-
-// This variable will be pre-programmed with our authentication key
-// (the one we received when we registered)
-var authKey = "0670364e971b486b99620ae260687e00";
-
-// These variables will hold the results we get from the user's inputs via HTML
-var searchTerm = "";
-var numResults = 0;
-
-var interestsInput = $("#interests-input");
-
-// queryURLBase is the start of our API endpoint. The searchTerm will be appended to this when
-// the user hits the search button
-var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-  authKey + "&q=" + interestsInput;
+//----- GLOBAL VARIABLES -----//
 
 // Counter to keep track of article numbers as they come in
 var articleCounter = 0;
 
-// FUNCTIONS
-// ==========================================================
+//----- FUNCTIONS -----//
 
 // This runQuery function expects two parameters:
 // (the number of articles to show and the final URL to download data from)
-function runQuery(numArticles, queryURL) {
+function runQuery(numArticles, queryURLBase) {
 
-  // The AJAX function uses the queryURL and GETS the JSON data associated with it.
-  // The data then gets stored in the variable called: "NYTData"
+  //AJAX call
  $.ajax({
-    url: queryURL,
+    url: queryURLBase,
     method: "GET"
   }).done(function(NYTData) {
 
-    // Logging the URL so we have access to it for troubleshooting
+    //Log queryURLBase and interestsInput
     console.log("------------------------------------");
-    console.log("URL: " + queryURL);
+    console.log("URL: " + queryURLBase);
     console.log("------------------------------------");
 
     // Log the NYTData to console, where it will show up as an object
+    console.log("------------------------------------");
     console.log(NYTData);
     console.log("------------------------------------");
+  
 
     // Loop through and provide the correct number of articles
     for (var i = 0; i < numArticles; i++) {
 
       // Add to the Article Counter (to make sure we show the right number)
-      articleCounter++;
+      //articleCounter++;
 
       // Create the HTML well (section) and add the article content for each
-      var newsselection = $("<div>");
-      newsselection.addClass("news1");
-      newsselection.attr("id", "article-news1" + articleCounter);
-      $("#newsselection").append(newsselection);
+      var newsSelection = $("<div>");
+      newsSelection.addClass("news" + [i]);
+      newsSelection.attr("id", "data-number" + [i]);
+      $("#news").append(newsSelection);
 
+      
       // Confirm that the specific JSON for the article isn't missing any details
       // If the article has a headline include the headline in the HTML
       if (NYTData.response.docs[i].headline !== "null") {
-        $("#article-news1" + articleCounter)
+        $("#data-number" + [i])
           .append(
             "<h3 class='articleHeadline'><span class='label label-primary'>" +
-            articleCounter + "</span><strong> " +
+            [i] + "</span><strong> " +
             NYTData.response.docs[i].headline.main + "</strong></h3>"
           );
 
@@ -68,9 +53,10 @@ function runQuery(numArticles, queryURL) {
         console.log(NYTData.response.docs[i].headline.main);
       }
 
+      /*
       // If the article has a byline include the headline in the HTML
       if (NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.original) {
-        $("#news1" + articleCounter)
+        $("#data-number" + [i])
           .append("<h5>" + NYTData.response.docs[i].byline.original + "</h5>");
 
         // Log the first article's Author to console.
@@ -78,7 +64,7 @@ function runQuery(numArticles, queryURL) {
       }
 
       // Then display the remaining fields in the HTML (Section Name, Date, URL)
-      $("#newsselection" + articleCounter)
+      $("#news" + [i])
         .append("<h5>Section: " + NYTData.response.docs[i].section_name + "</h5>");
       $("#newsselection" + articleCounter)
         .append("<h5>" + NYTData.response.docs[i].pub_date + "</h5>");
@@ -92,7 +78,9 @@ function runQuery(numArticles, queryURL) {
       console.log(NYTData.response.docs[i].pub_date);
       console.log(NYTData.response.docs[i].section_name);
       console.log(NYTData.response.docs[i].web_url);
+    */
     }
+
   });
 
 }
@@ -102,36 +90,34 @@ function runQuery(numArticles, queryURL) {
 
 // on.("click") function associated with the Search Button
 $("#run-search").on("click", function(event) {
-  // This line allows us to take advantage of the HTML "submit" property
-  // This way we can hit enter on the keyboard and it registers the search
-  // (in addition to clicks).
+  //prevents form from pushing thorugh on page load
   event.preventDefault();
 
-  var interestsInput = $("#interests-input");
-  console.log("-----"+ interestsInput +"-----");
+  // Empties the region associated with the articles
+  $("#news").clear();
 
   // Initially sets the articleCounter to 0
   articleCounter = 0;
 
-  // Empties the region associated with the articles
-  $("#news").empty();
+  //API key
+  var authKey = "0670364e971b486b99620ae260687e00";
 
   // Grabbing text the user typed into the search input
+  var interestsInput = $("#interests-input");
+  console.log("_--------asdfasdfas:" + interestsInput);
 
-  searchTerm = $("#interests-input").val().trim();
-  var queryURL = queryURLBase + searchTerm;
+  //queryURL
+  var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+  authKey + "&q=" + interestsInput;
 
   // Number of results the user would like displayed
-  numResults = 5;
+  numArticles = 5;
 
- 
   // Then we will pass the final queryURL and the number of results to
   // include to the runQuery function
-  runQuery(numResults, queryURL);
-});
+  runQuery(numArticles, queryURLBase);
 
-// This button clears the top articles section
-$("#clear-all").on("click", function() {
-  articleCounter = 0;
-  $("#newsselection").empty();
+  
+  
+
 });
