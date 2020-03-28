765507cedd374cf47596f8cd8bb17a76fc754dbd
Bobby Angus <Bobby Angus>
2020-03-28 17:01:29 -0400

    changes

--- assets/javascript/indexJavascript.js
+++ assets/javascript/indexJavascript.js
@@ -31,27 +31,7 @@ $("#run-search").on("click", function(event){
  });
 
 
-// // // --------- Authentication code ------ // // //
-// Track the UID of the current user.  
-     var currentUid = null;  
-     firebase.auth().onAuthStateChanged(function(user) {  
-      // onAuthStateChanged listener triggers every time the user ID token changes.  
-      // This could happen when a new user signs in or signs out.  
-      // It could also happen when the current user ID token expires and is refreshed.  
-      if (user && user.uid != currentUid) {  
-       // Update the UI when a new user signs in.  
-       // Otherwise ignore if this is a token refresh.  
-       // Update the current user UID.  
-       currentUid = user.uid;
-       console.log(user.uid); 
-       // document.body.innerHTML = '<h1> Congrats ' + user.displayName + ', you are done! </h1> <h2> Now get back to what you love building. </h2> <h2> Need to verify your email address or reset your password? Firebase can handle all of that for you using the email you provided: ' + user.email + '. <h/2>';  
-      } else {  
-       // Sign out operation. Reset the current user UID.  
-       currentUid = null;  
-       console.log("no user signed in");  
-       window.location.href = "signin.html";
-      }  
-     });  
+
      
 
 // // //---------- FUNCTIONS ----------// // //
@@ -177,7 +157,7 @@ function populateNYT(volunteer) {
   var numArticles = 10;
 
   //API key
-  var authKey = "0670364e971b486b99620ae260687e00";
+  var authKey = "ZPZtr0I8LB2UFHOF8FOVPd34QnKwaJCu";
 
   // Grabbing text the user typed into the search input
   //var volunteer = $("#interests-input").val().trim();
